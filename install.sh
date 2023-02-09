7badf0f0facc5ecb4ed8d185ef3105150c426d40
Szymon Gamża <40543244+szymongamza@users.noreply.github.com>
2023-02-09 16:17:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 sudo cp /home/pi/CM_script/splash.png /root/
+sudo cp /home/pi/CM_script/config.txt /root/
 cp /home/pi/CM_script/start_chromium_browser /home/pi/scripts/
 read -p "Wprowadz url strony: " url
 echo $url | sudo tee /boot/fullpageos.txt
